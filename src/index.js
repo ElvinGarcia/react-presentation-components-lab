dab729275b6da0d94f9a03ccaf5c857dfb6d7dc5
Elvin <Elvin@ElvinGarcia.com>
2022-03-24 22:48:53 -0400

    done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=>alert('😆😆😆😆😆😆😆😆😆😆😆😆😆😆😆😆😆😆')} />
   </div>,
   document.getElementById('root')
 );
